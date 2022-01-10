ae072e4c9edfca53de0a44ce401737847421a5e2
whitegoldukhta <james.99@list.ru>
2022-01-10 17:54:11 +0200

    delete comments

--- CoreTaskTemplate1/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate1/src/main/java/jm/task/core/jdbc/Main.java
@@ -22,6 +22,6 @@ public class Main {
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
-    }//end main
-}//end Main
+    }
+}
 
